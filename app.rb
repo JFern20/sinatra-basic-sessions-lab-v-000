cf9df432f15407caf67bbaf68524904e77062818
Jonathan Fernandez <jfern20@gmail.com>
2021-01-24 06:22:55 +0000

    Automatically backed up by Learn

--- app.rb
+++ app.rb
@@ -5,17 +5,18 @@ class App < Sinatra::Base
 
   configure do
     enable :sessions
-    set :session_secret, 'flatiron'
+    set :session_secret, 'secret'
   end
 
-  get '/' do
-    erb :index
-  end
+get '/' do 
+ erb :index
+end 
 
-  post '/checkout' do 
-    session[:item] = params[:item]
-    @session = session
-    erb :checkout
-  end
+ post '/add_to_cart' do 
+   session[:item] = params[:item]
+   @session = session 
+   erb :add_to_cart
+ end 
+
 
 end
